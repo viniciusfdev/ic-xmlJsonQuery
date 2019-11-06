450563a349b9c55bed738d4539ece32036dc10b7
Vinicius <vinilima07@gmail.com>
2019-11-06 14:22:29 -0300

    complete query generator

---
+++ QueryGenerator/src/Generator.java
@@ -0,0 +1,83 @@
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Random;
+import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+import org.xml.sax.XMLReader;
+import org.xml.sax.helpers.XMLReaderFactory;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author vinicius
+ */
+public class Generator {
+    List<String> tokens_ = new ArrayList<>();
+    
+    public void run(int nQuery, int nTokens, String queryFileName, String baseName){
+        String line = "";
+        Random rand = new Random();
+        BufferedWriter queryFile = null;
+        File folder = new File("src/xml/"+baseName);
+        File listOfFiles[] = folder.listFiles();
+        Set<String> tokens = new HashSet<>();
+        
+        
+        try {
+            queryFile = new BufferedWriter(new FileWriter(new 
+                File("src/query_test/"+queryFileName).getAbsolutePath()));
+            
+            for(File file: listOfFiles){
+      
+                try {
+                    XMLReader xr = XMLReaderFactory.createXMLReader();
+                    Tokenizer tokenizer = new Tokenizer();
+                    xr.setContentHandler(tokenizer);
+                    xr.setErrorHandler(tokenizer);
+                    xr.parse(new InputSource(new FileReader(file)));
+                    tokens.addAll(tokenizer.getTokens());
+                } catch (SAXException ex) {
+                    System.out.println(ex.getCause());
+                    System.out.println(ex.getMessage());
+                }
+                
+            }
+            
+            tokens_.addAll(tokens);
+            int i;
+            for(i = 0 ; i < nQuery ; i++){
+                String query = "";
+                for(int j = 0; j < nTokens ; j++){
+                    int pos = rand.nextInt(tokens.size());
+                    while(query.contains(tokens_.get(pos))){
+                        pos = rand.nextInt(tokens.size());
+                    }
+                    query += "::"+tokens_.get(pos)+" ";
+                }
+                System.out.println("Query("+(i+1)+") = "+query);
+                queryFile.write(query+"\n");
+            }
+            queryFile.close();
+            
+            //System.out.println(tokens);
+        } catch (IOException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+    }
+}
--- QueryGenerator/src/Main.java
+++ QueryGenerator/src/Main.java
@@ -4,7 +4,10 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
+import java.util.Random;
 import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -30,43 +33,15 @@ public class Main  extends DefaultHandler{
      * @param args the command line arguments
      */
     public static void main(String[] args){
-
-        String line = "";
-        int nTperQuery = 4;
-        BufferedWriter queryFile = null;
-        File folder = new File("src/xml/");
-        File listOfFiles[] = folder.listFiles();
-        Set<String> tokens = new HashSet<>();
-        String queryFileName = "GEN_query_test_1.txt";
-        
-        try {
-            queryFile = new BufferedWriter(new FileWriter(new File("src/query/"+queryFileName).getAbsolutePath()));
-            
-            for(File file: listOfFiles){
-      
-                try {
-                    XMLReader xr = XMLReaderFactory.createXMLReader();
-                    Tokenizer tokenizer = new Tokenizer();
-                    xr.setContentHandler(tokenizer);
-                    xr.setErrorHandler(tokenizer);
-                    xr.parse(new InputSource(new FileReader(file)));
-                    tokens.addAll(tokenizer.getTokens());
-                } catch (SAXException ex) {
-                    System.out.println(ex.getCause());
-                    System.out.println(ex.getMessage());
-                }
-                
-            }
-            
-            for(String token: tokens){
-            }
-            
-            System.out.println(tokens);
-        } catch (IOException ex) {
-            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        
+        Generator gen = new Generator();
+        //1 - n queries
+        //2 - n tokens
+        //3 - query file name
+        //4 - base name(directory name)
+        gen.run(1000, 4, "GEN_query_test_1.txt", "");
     }
+    
+    
     
 }
 
