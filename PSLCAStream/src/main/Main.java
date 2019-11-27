687b8a964702b1e337af9b4dbaedff23c71cb421
Vinicius <vinilima07@gmail.com>
2019-11-27 15:40:52 -0300

    update tesr script

--- PSLCAStream/src/main/Main.java
+++ PSLCAStream/src/main/Main.java
@@ -9,10 +9,9 @@ import java.io.File;
 public class Main {
 
     public static void main(String[] args) {
-        // TODO code application logic here
         boolean semantic = true;
-        String baseName = args[0];
-        
+
+//        String baseName = args[0];
 //        int nGroups = Integer.parseInt(args[2]);    
 //        int nThreads = Integer.parseInt(args[1]);
 //        int nQueries = Integer.parseInt(args[0]);
@@ -22,15 +21,22 @@ public class Main {
         int nQueries = 50000;
         //String baseName = "xmark";
         
-        File folder = new File("xml/"+baseName+"/");
-        String queryFileName = baseName+"_test_1_4.txt";
-        File listOfFiles[] = folder.listFiles();
+        
+        String []bases = {"xmark", "isfdb", "sigmod"};
         //true = SLCA
         //false = ELCA
-        for(int j = 10000; j <= nQueries ; j = j+10000){
-            for(int i = 1; i <= nThreads; i = i*2){
-                QueryProcessor qp = new QueryProcessor(queryFileName, listOfFiles, semantic, j, i, 1);
-                qp.multipleQueriesStart();
+        for(String baseName: bases){
+            String queryFileName = baseName+"_test_";
+            File folder = new File("xml/"+baseName+"/");
+            File listOfFiles[] = folder.listFiles();
+            for(int q = 0; q < 4 ; q++){
+                queryFileName += q+"l5t_50000.txt";
+                for(int j = 10000; j <= nQueries ; j = j+10000){
+                    for(int i = 1; i <= nThreads; i = i*2){
+                        QueryProcessor qp = new QueryProcessor(queryFileName, listOfFiles, semantic, j, i, 1);
+                        qp.multipleQueriesStart();
+                    }
+                }
             }
         }
 //        QueryProcessor qp = new QueryProcessor(queryFileName, listOfFiles, semantic, 10000, 1, 1);
